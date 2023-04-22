9fe96f08fab308eb19aec15b4a9f31f1037e0eef
trtovacasmir <trtovacasmir@gmail.com>
2023-04-22 22:17:45 +0200

    signup edit

---
+++ js/signup.js
@@ -0,0 +1,36 @@
+const name = document.getElementById('name')
+const last = document.getElementById('last')
+const email = document.getElementById('email')
+const address = document.getElementById('address')
+const username = document.getElementById('username')
+const password = document.getElementById('password')
+
+function SignUp() {
+    fetch('https://fakestoreapi.com/users',{
+            method:"POST",
+            body:JSON.stringify(
+                {
+                    email:'John@gmail.com',
+                    username:'johnd',
+                    password:'m38rmF$',
+                    name:{
+                        firstname:'John',
+                        lastname:'Doe'
+                    },
+                    address:{
+                        city:'kilcoole',
+                        street:'7835 new road',
+                        number:3,
+                        zipcode:'12926-3874',
+                        geolocation:{
+                            lat:'-37.3159',
+                            long:'81.1496'
+                        }
+                    },
+                    phone:'1-570-236-7033'
+                }
+            )
+        })
+            .then(res=>res.json())
+            .then(json=>console.log(json))
+}
